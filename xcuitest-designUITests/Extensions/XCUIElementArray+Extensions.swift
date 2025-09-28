ca37f3b3e16ad095f625b66bfd900827f21e7868
Jason Dobo <jason.dobo@gmail.com>
2025-09-28 01:01:52 +0100

    Improve XCUIElementArray+Extensions

--- xcuitest-designUITests/Extensions/XCUIElementArray+Extensions.swift
+++ xcuitest-designUITests/Extensions/XCUIElementArray+Extensions.swift
@@ -13,43 +13,39 @@ import XCTest
 extension Array where Element: XCUIElement {
     
     /**
-     wait for all XCUIElements to match the expected ElementState and return a bool
-     - Parameters
-        - `state`: expected element state
-        - `timeout`: time to wait for state to match
-     - Returns: `true` if all elements successfully reach the `state` within the timeout else return `false`
+     Wait for all XCUIElements to match the expected ElementState and return a bool.
+     - Parameters:
+        - state: Expected element state. Must be a valid NSPredicate format string.
+        - timeout: Time to wait for state to match
+     - Returns: `true` if all elements successfully reach the `state` within the timeout, else `false`
      */
-
     @discardableResult
     func waitForAllElements(with state: ElementState, timeout: TimeInterval = .loading) -> Bool {
         guard !self.isEmpty else {
             return false
         }
         
-        let myPredicate = NSPredicate (format: state.rawValue)
-        var allExpectations: [XCTNSPredicateExpectation]!
-        allExpectations = self.map { element -> XCTNSPredicateExpectation in
-            return XCTNSPredicateExpectation (predicate: myPredicate, object: element)
-        }
+        let predicate = NSPredicate (format: state.rawValue)
+        let expectations = self.map { XCTNSPredicateExpectation(predicate: predicate, object: $0) }
         
-        let result = XCTWaiter().wait(for: allExpectations, timeout: timeout, enforceOrder: false)
-        return result == XCTWaiter.Result.completed
+        let result = XCTWaiter.wait(for: expectations, timeout: timeout)
+        return result == .completed
     }
     
     /**
-     wait for first XCUIElement to match the expected ElementState and return the match
-     - Parameters
-        - `state`: expected element state
-        - `timeout`: time to wait for state to match
+     Wait for first XCUIElement to match the expected ElementState and return the match.
+     Exits as soon as any match occurs, fulfilling all expectations.
+     - Parameters:
+        - state: Expected element state. Must be a valid NSPredicate format string.
+        - timeout: Time to wait for state to match
+        - handler: Closure to call with the matched element
      - Returns: The `XCUIElement` that first matched the state within the timeout else return `nil`
      */
     @discardableResult
     func waitForFirstElement(with state: ElementState,
                              timeout: TimeInterval = .loading,
                              handler: ((XCUIElement) -> Void)? = nil) -> XCUIElement? {
-        guard !self.isEmpty else {
-            return nil
-        }
+        guard !self.isEmpty else { return nil }
         
         let myPredicate = NSPredicate(format: state.rawValue)
         var allExpectations: [XCTNSPredicateExpectation]!
@@ -77,6 +73,42 @@ extension Array where Element: XCUIElement {
             handler?(matchedElement)
         }
         
+        return matchedElement
+    }
+    
+    /**
+     Wait for first XCUIElement to match the expected ElementState and return the match.
+     Exits as soon as any match occurs, fulfilling all expectations.
+     - Parameters:
+        - state: Expected element state. Must be a valid NSPredicate format string.
+        - timeout: Time to wait for state to match
+        - handler: Closure to call with the matched element
+     - Returns: The `XCUIElement` that first matched the state within the timeout else return `nil`
+     */
+    func waitForFirst(with state: ElementState,
+                             timeout: TimeInterval = .loading,
+                             handler: ((XCUIElement) -> Void)? = nil) -> XCUIElement? {
+        guard !self.isEmpty else { return nil }
+        
+        let predicate = NSPredicate(format: state.rawValue)
+        var matchedElement: XCUIElement?
+        var expectations: [XCTNSPredicateExpectation] = []
+        
+        expectations = self.map { element in
+            let expectation = XCTNSPredicateExpectation(predicate: predicate, object: element)
+            expectation.handler = {
+                if matchedElement == nil {
+                    matchedElement = element
+                    expectations.forEach { $0.fulfill() }
+                    return true
+                }
+                return false
+            }
+            return expectation
+        }
+                
+        _ = XCTWaiter.wait(for: expectations, timeout: timeout)
+        if let element = matchedElement { handler?(element) }
         return matchedElement
     }
 }
